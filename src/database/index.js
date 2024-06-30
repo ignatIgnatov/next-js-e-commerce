8d001de39712ce8e234b80c30514c9b93241e23b
ignatIgnatov <ignatov_rs@abv.bg>
2024-06-30 16:20:15 +0300

    implement mongoDb database connection

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const configOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+
+const connectToDB = async () => {
+    const connectionUrl = process.env(DATABASE_URL);
+
+    mongoose.connect(connectionUrl, configOptions)
+        .then(() => console.log('Ecommerce database connected successfully!'))
+        .catch((error) => console.log(`Error from DB connection: ${error.mongoose}`))
+}
+
+export default connectToDB;
