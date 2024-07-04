59c406b8c6e66f310302a7de9f83be238f01641d
ignatIgnatov <ignatov_rs@abv.bg>
2024-07-04 13:20:32 +0300

    implement the service for registration of new user

---
+++ src/services/register/index.js
@@ -0,0 +1,18 @@
+export const registerNewUser = async (formdata) => {
+    try {
+
+        const response = await fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(formdata)
+        });
+
+        const finalData = response.json();
+        return finalData;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
