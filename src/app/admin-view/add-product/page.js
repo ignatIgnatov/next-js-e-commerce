2e428fa7a375e43526754ff34ab9eb7245880193
ignatIgnatov <ignatov_rs@abv.bg>
2024-07-07 13:21:29 +0300

    implement add product frontend

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -1,8 +1,57 @@
 
+'use client'
+
+import InputComponent from "@/components/FormElements/InputComponent"
+import SelectComponent from "@/components/FormElements/SelectComponent"
+import TileComponent from "@/components/FormElements/TileComponent"
+import { AvailableSizes, adminAddProductformControls } from "@/utils"
+
+
 
 const AdminAddNewProduct = () => {
+
+  const handleImage = () => {
+
+  }
+
   return (
-    <div>AdminAddNewProduct</div>
+    <div className="w-full mt- mr-0 mb-0 ml-0 relative">
+      <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative">
+        <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8">
+          <input
+            accept="image/*"
+            max={'1000000'}
+            type="file"
+            onChange={handleImage}
+          />
+
+          <div className="flex gap-6 flex-col">
+            <label>Available sizes</label>
+            <TileComponent data={AvailableSizes} />
+            {
+              adminAddProductformControls.map(controlItem =>
+              (
+                controlItem.componentType === 'input' ?
+                  <InputComponent
+                    type={controlItem.type}
+                    placeholder={controlItem.placeholder}
+                    label={controlItem.label}
+                  /> :
+                  controlItem.componentType === 'select' ?
+                    <SelectComponent
+                      label={controlItem.label}
+                      options={controlItem.options}
+                    /> : null
+              )
+              )
+            }
+            <button
+              className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white font-medium uppercase tracking-wide"
+            >Add Product</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
