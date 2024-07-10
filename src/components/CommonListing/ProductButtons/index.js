22ce4df6772939ae0499c2fa1088e8950111d889
ignatIgnatov <ignatov_rs@abv.bg>
2024-07-10 14:46:45 +0300

    implement cart service and connect cart backend with frontend

--- src/components/CommonListing/ProductButtons/index.js
+++ src/components/CommonListing/ProductButtons/index.js
@@ -40,27 +40,27 @@ const ProductButtons = ({ item }) => {
         }
     }
 
-    // async function handleAddToCart(getItem) {
-    //     setComponentLevelLoader({ loading: true, id: getItem._id });
+    const handleAddToCart = async (getItem) => {
+        setComponentLevelLoader({ loading: true, id: getItem._id });
 
-    //     const res = await addToCart({ productID: getItem._id, userID: user._id });
+        const res = await addToCart({ productID: getItem._id, userID: user._id });
 
-    //     if (res.success) {
-    //         toast.success(res.message, {
-    //             position: toast.POSITION.TOP_RIGHT,
-    //         });
-    //         setComponentLevelLoader({ loading: false, id: "" });
-    //         setShowCartModal(true);
-    //     } else {
-    //         toast.error(res.message, {
-    //             position: toast.POSITION.TOP_RIGHT,
-    //         });
-    //         setComponentLevelLoader({ loading: false, id: "" });
-    //         setShowCartModal(true)
-    //     }
+        if (res.success) {
+            toast.success(res.message, {
+                position: 'top-right',
+            });
+            setComponentLevelLoader({ loading: false, id: "" });
+            setShowCartModal(true);
+        } else {
+            toast.error(res.message, {
+                position: 'top-right',
+            });
+            setComponentLevelLoader({ loading: false, id: "" });
+            setShowCartModal(true)
+        }
 
-    //     console.log(res);
-    // }
+        console.log(res);
+    }
 
     return isAdminView ? (
         <>
@@ -93,7 +93,7 @@ const ProductButtons = ({ item }) => {
     ) : (
         <>
             <button
-
+                onClick={() => handleAddToCart(item)}
                 className="mt-1.5 flex w-full justify-center bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
             >
                 {componentLevelLoader &&
